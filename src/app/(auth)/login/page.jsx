8faf101390959f9b806299e0266c8e745a019d78
Harsh Patwa <harsh.patwa@systango.com>
2025-01-10 17:57:03 +0530

    login form

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,105 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(userName, password);
+    if(userName === "admin" && password === "admin123"){
+        sessionStorage.setItem("userId", "admin");
+        router.push("/");
+    }
+    else{setErrorMessage("Invalid username or password.")}
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <div className="flex bg-gray-50 min-h-full flex-1 flex-col justify-center py-6 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            alt="Your Company"
+            src="https://www.systango.com/img/mega-menu-icon/systango-color.svg"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="pt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"></h2>
+        </div>
+
+        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div>
+                <label
+                  htmlFor="userName"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="userName"
+                    name="userName"
+                    type="userName"
+                    required
+                    placeholder="admin"
+                    autoComplete="userName"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="admin123"
+                    required
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              {errorMessage && (
+                <p className="text-red-500 text-sm" aria-live="assertive">
+                  {errorMessage}
+                </p>
+              )}
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  {loading ? "Signing In..." : "Sign In"}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
